eee746ba1f98f2f7e80927b574ebc4b6fb86a611
Marcin Stangel <mstangel@splunk.com>
2024-02-27 15:34:26 +0000

    adding bash script and payload

--- make_moo.sh
+++ make_moo.sh
@@ -3,15 +3,19 @@
 #detect os family. Supports Deb and RPM
 
 osFamily=$(uname -a|awk '{print $1}')
-
-#echo $osFamily
+OS="none"
+CowBin="none"
 
 if [ "$osFamily" == "Darwin" ]; then
-	echo "MACOS is unsupported"
+	if [ -f /usr/local/bin/brew ] ; then
+		if [ ! -f /usr/local/bin/cowsay ]; then
+			brew install cowsay
+		fi;
+		CowBin="/usr/local/bin/cowsay"
+	else
+		echo "HOMEBREW Required to install cowsay binary"
+	fi;
 elif [ "$osFamily" == "Linux" ]; then
-	echo "Checking Distro..."
-	OS="none"
-	CowBin="none"
 	if [ -f /etc/os-release ]; then
 		OS=$(cat /etc/os-release |grep ^NAME|awk '{print $1}'|sed -e 's/\(.*\)=\"//')
 		#DEBIAN and CENTOS
@@ -25,17 +29,18 @@ elif [ "$osFamily" == "Linux" ]; then
 		sudo yum -y -q install epel-release && yum -y -q install cowsay
 		CowBin="/usr/bin/cowsay"
 	fi;
-
-	COWSTRING="MOO MOO MOO MOO MOO"
-
-
-	clear
-	x=1
-	while [ $x -le 10 ]; do 
-		${CowBin} ${COWSTRING}
-		((x++))
-	done
-
-
 fi;
+
+COWSTRING="MOO MOO MOO MOO MOO"
+
+
+clear
+x=1
+while [ $x -le 10 ]; 
+do 
+	${CowBin} ${COWSTRING}
+	((x++))
+done
+
+
 
